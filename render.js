991717023edec83b0eb3ef2ea550344b1d23278c
Emma Koslow <emmakoslowacea@gmail.com>
2021-02-20 15:13:33 -0500

    hello

--- render.js
+++ render.js
@@ -8,6 +8,8 @@ var open = require('open');
 var fs = require('fs');
 
 
+
+
 var port = 3000;
 var app = express();
 
@@ -22,6 +24,8 @@ minors+= jsonData[1].minor[i] + ", ";
 var html = "<html><head> <title>" + jsonData[1].first_name +  "house </title> </head> <body style='background-color:teal'>" + jsonData[0].first_name + " " + jsonData[0].last_name + "'s website <br> <br>" +  " "+  jsonData[0].school+ "<br> <br>" + jsonData[0].major + " major with minors in " + minors + "</body> </html>"
 
 app.post('/', function(request, respond) {
+
+
     fs.writeFile('message.html', html, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
