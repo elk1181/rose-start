4363054fd14bb5aae9a580061764bb22dc22e927
Emma Koslow <emmakoslowacea@gmail.com>
2021-02-20 20:51:34 -0500

    yayyy

--- render.js
+++ render.js
@@ -28,34 +28,189 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'information.html'));
 });
 
-var minors = "";
-for(var i=0; i<jsonData[1].minor.length; i++){
-minors+= jsonData[1].minor[i] + ", ";
-}
-var html = "<html><head> <title>" + jsonData[1].first_name +  "house </title> </head> <body style='background-color:teal'>" + jsonData[0].first_name + " " + jsonData[0].last_name + "'s website <br> <br>" +  " "+  jsonData[0].school+ "<br> <br>" + jsonData[0].major + " major with minors in " + minors + "</body> </html>"
+
+
+
+
+
+
+
+
 
 app.post('/', function(request, respond) {
-    var minors = "";
-    if(Array.isArray(request.body.minor)){
+
+//String construction stuff
+
+ 
+var minors = "<p style=text-align:center;>";
+if(Array.isArray(request.body.minor)){
     for(var i=0; i<request.body.minor.length; i++){
     minors+= request.body.minor[i] + ", ";
     }
+
+
+
 }
 else{
     minors=request.body.minor;
 }
-    var htmlE = "<html><head> <title>" + request.body.first_name +  "house </title> </head> <body style='background-color:teal'>" + request.body.first_name + " " + request.body.last_name + "'s website <br> <br>" +  " "+  request.body.school+ "<br> <br>" + request.body.major + " major with minors in " + minors + "</body> </html>"
-    
-        console.log(request.body);
-      
-     
+minors += "</p>";
 
-    fs.writeFile('message.html', htmlE, (err) => {
+var majors = "<p style=text-align:center;>";
+if(Array.isArray(request.body.major)){
+    for(var i=0; i<request.body.major.length; i++){
+    majors+= request.body.major[i] + ", ";
+    }
+
+
+
+}
+else{
+    majors=request.body.major;
+}
+majors += "</p>";
+
+var webSkills = "<p style=text-align:center;>";
+if(Array.isArray(request.body.skills)){
+for(var i=0; i<request.body.skills.length; i++){
+    if (i == request.body.skills.length-1){
+        webSkills+= request.body.skills[i];
+    }
+    else {
+        webSkills+= request.body.skills[i] + ", ";
+    }
+}
+}
+else{
+    webSkills+=request.body.skills;
+}
+webSkills += "</p>";
+
+var webLanguages = "<p style=text-align:center;>";
+if(Array.isArray(request.body.languages)){
+for(var i=0; i<request.body.languages.length; i++){
+    if (i == request.body.languages.length-1){
+        webLanguages+= request.body.languages[i];
+    }
+    else {
+        webLanguages+= request.body.languages[i] + ", ";
+    }
+}
+}
+else{
+    webLanguages+=request.body.languages;
+}
+webLanguages += "</p>";
+
+var webAwards = "<p style=text-align:center;>";
+if(Array.isArray(request.body.awards)){
+for(var i=0; i<request.body.awards.length; i++){
+    if (i == request.body.awards.length-1){
+        webAwards+= request.body.awards[i];
+    }
+    else {
+        webAwards+= request.body.awards[i] + ", ";
+    }
+}
+}
+else{
+    webAwards+=request.body.awards;
+}
+webAwards += "</p>";
+
+//Generated website helper functions
+let h2titles = function(title){
+    return ("<h2 style='font-size: 40px; text-align:center;'>" + title + "</h2>")
+}
+
+let webJobs = function(){
+    let arr = [];
+if(Array.isArray(request.body["work_experience"]["'company_name'"])){
+    for(var i=0;i<request.body["work_experience"]["'company_name'"].length;i++){
+        arr.push("<h2 style='text-align:center;'>" + request.body["work_experience"]["'company_name'"][i]+", " + request.body["work_experience"]["'position'"][i] + "</h2>"
+        + "<p style='font-style:italic;text-align:center;'>" + request.body["work_experience"]["'start_date'"][i] + " - " + request.body["work_experience"]["'end_date'"][i] + "</p>"
+        + "<p style='text-align:center;'>" + request.body["work_experience"]["'job_description'"][i]+ "</p>");
+    }
+}
+else{
+     arr.push("<h2 style='text-align:center;'>" + request.body["work_experience"]["'company_name'"]+", " + request.body["work_experience"]["'position'"] + "</h2>"
+        + "<p style='font-style:italic;text-align:center;'>" + request.body["work_experience"]["'start_date'"] + " - " + request.body["work_experience"]["'end_date'"] + "</p>"
+        + "<p style='text-align:center;'>" + request.body["work_experience"]["'job_description'"] + "</p>");
+}
+    return arr;
+}
+
+let floatingBox = function(firstName, lastName, email){
+    return "<h1 style='float:right;background-color:#87CEFA;position:fixed;padding:10px;text-align:center;'>" + firstName + " " + lastName + "<p style='font-style:italic;font-size:20px'>" + email + "</h1>";
+}
+
+let webProjects = function(){
+    let arr = [];  
+if(Array.isArray(request.body["projects"]["'project_name'"])){
+  
+     for(var i=0;i<request.body["projects"]["'project_name'"].length;i++){
+                arr.push("<h2 style='text-align:center;'>" + request.body["projects"]["'project_name'"][i] + "</h2>"
+                + "<p style='font-style:italic;text-align:center;'>" + request.body["projects"]["'date'"][i] + "</p>"
+                + "<p style='text-align:center;'>" + request.body["projects"]["'description'"][i] + "</p>");
+            }
+
+
+
+}
+else{
+    arr.push("<h2 style='text-align:center;'>" + request.body["projects"]["'project_name'"] + "</h2>"
+                + "<p style='font-style:italic;text-align:center;'>" + request.body["projects"]["'date'"] + "</p>"
+                + "<p style='text-align:center;'>" + request.body["projects"]["'description'"] + "</p>");
+}
+   
+    return arr;
+}
+
+let webActivities = function(){
+    let arr = [];
+if(Array.isArray(request.body["activities"]["'activity_name'"])){
+    for(var i=0;i<request.body["activities"].length;i++){
+        arr.push("<h2 style='text-align:center;'>" + request.body["activities"]["'activity_name'"][i] + "</h2>"
+        + "<p style='font-style:italic;text-align:center;'>" + request.body["activities"]["'start_date'"][i] + "-" + request.body["activities"]["'end_date'"][i] + "</p>"
+        + "<p style='text-align:center;'>" + request.body["activities"]["'description'"][i] + "</p>");
+    }
+}
+else{
+     arr.push("<h2 style='text-align:center;'>" + request.body["activities"]["'activity_name'"] + "</h2>"
+        + "<p style='font-style:italic;text-align:center;'>" + request.body["activities"]["'start_date'"] + "-" + request.body["activities"]["'end_date'"] + "</p>"
+        + "<p style='text-align:center;'>" + request.body["activities"]["'description'"] + "</p>");
+}
+    return arr;
+}
+
+
+//Full css for generated website
+var htmlE = "<html><head> <title>" + request.body.first_name + 
+"house </title> </head> <body style='background-color:#fff0f5;'><h1 style='text-align:center;border-style:outset;padding:5px;background-color:#87CEFA;'>"
++ request.body.first_name + " " + request.body.last_name + "'s website" +
+"<p style='font-style:italic;font-size:75%'>" + request.body.email +
+"</p><h2 style ='text-align:center;'>" + request.body.school +
+"</h2><h3 style='text-align:center;font-style:italic;'>" + request.body.major +
+" major with minors in " + minors + "</h3>" + "</h1>" +
+floatingBox(request.body.first_name, request.body.last_name, request.body.email) + 
+h2titles("Experience") + webJobs() +
+h2titles("Projects") + webProjects() +
+h2titles("Activities") + webActivities() +
+h2titles("Skills") + webSkills +
+h2titles("Languages") + webLanguages +
+h2titles("Awards") + webAwards + "</body> </html>"
+
+
+        console.log(request.body);
+        console.log(request.body["projects"]["'project_name'"]);
+     
+        const file = request.body.last_name + '.html';
+    fs.writeFile(file, htmlE, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
 
 
-const file = `message.html`;
+
 respond.download(file); // Set disposition and send it.
 
     });
@@ -161,6 +316,7 @@ app.post('/pdf', (req, res, next)=>{
 
     //const fname = req.body.fname;
     //const lname = req.body.lname;
+    
 
     var documentDefinition = {
         content: [
